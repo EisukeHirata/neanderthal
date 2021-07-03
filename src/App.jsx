acddf3d2a23abc32b55be64b04fabff7824fc6f7
EisukeHirata <82617733+EisukeHirata@users.noreply.github.com>
2021-07-02 23:16:39 -0700

    add Top Page

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <body>
+      <header>
+        <h1>Tech House Community</h1>
+      </header>
+      <nav>
+        <ul className="nav">
+          <li>Home</li>
+          <li>about us</li>
+          <li>Posts</li>
+          <li>Database</li>
+        </ul>
+      </nav>
+      <main></main>
+    </body>
+  );
+}
